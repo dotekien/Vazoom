2814ac458d19968af3b5d4a0d8a14d81da0cea6d
dotekien <dotekien@gmail.com>
2015-07-11 17:21:30 -0400

    Initial Commit

---
+++ Vazoom/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Vazoom
+//
+//  Created by Kien Do on 7/11/15.
+//  Copyright (c) 2015 Vazoom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ VazoomTests/VazoomTests.m
@@ -0,0 +1,40 @@
+//
+//  VazoomTests.m
+//  VazoomTests
+//
+//  Created by Kien Do on 7/11/15.
+//  Copyright (c) 2015 Vazoom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface VazoomTests : XCTestCase
+
+@end
+
+@implementation VazoomTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
